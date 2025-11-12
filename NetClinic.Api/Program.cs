e2c23067433f48c2e408e2720588dbdf28b71810
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2025-11-11 21:31:21 -0800

    Turned Program into class

--- NetClinic.Api/Program.cs
+++ NetClinic.Api/Program.cs
@@ -1,31 +1,39 @@
 using NetClinic.Api;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace NetClinic.Api;
 
-// Configure logging
-builder.Logging.ClearProviders();
-builder.Logging.AddConsole();
-builder.Logging.AddDebug();
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-builder.Services.AddControllers();
-
-var app = builder.Build();
-
-// Add request logging middleware
-app.UseMiddleware<RequestLoggingMiddleware>();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.MapOpenApi();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Configure logging
+        builder.Logging.ClearProviders();
+        builder.Logging.AddConsole();
+        builder.Logging.AddDebug();
+
+        // Add services to the container.
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddOpenApi();
+        builder.Services.AddControllers();
+
+        var app = builder.Build();
+
+        // Add request logging middleware
+        app.UseMiddleware<RequestLoggingMiddleware>();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+
+        // Won't use HTTPS directly
+        // app.UseHttpsRedirection();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-// Won't use HTTPS directly
-// app.UseHttpsRedirection();
-
-app.MapControllers();
-
-app.Run();
