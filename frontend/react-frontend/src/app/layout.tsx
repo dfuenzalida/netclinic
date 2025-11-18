beb3a53502b2152147505fa3f5e9bf80f96a4ef9
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2025-11-17 21:43:31 -0800

    Use font-awesome 4.7.0

--- frontend/react-frontend/src/app/layout.tsx
+++ frontend/react-frontend/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "@fortawesome/fontawesome-free/css/all.min.css";
+import "font-awesome/css/font-awesome.min.css";
 import "./globals.css";
 
 export const metadata: Metadata = {
