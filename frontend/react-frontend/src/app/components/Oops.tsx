c123480c5dd1068b9c1c1c90aa55a27aa2d57059
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2025-11-19 22:15:34 -0800

    Implemented Oops front-end component

--- frontend/react-frontend/src/app/components/Oops.tsx
+++ frontend/react-frontend/src/app/components/Oops.tsx
@@ -1,9 +1,52 @@
+import { useEffect, useState } from "react";
+
 export default function Oops() {
+
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchOops = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(`/api/oops`);
+      
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      
+    } catch (err) {
+      setError(err instanceof Error ? err.message : 'An error occurred');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchOops();
+  }, []); // Add currentPage as dependency to refetch when it changes
+
+  if (loading) {
+    return (
+      <div>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div>
+        <img src="images/pets.png" />
+          <h2>Something happened...</h2>
+          <p></p>
+      </div>
+    );
+  }
+
+
   return (
     <div>
-      <h2>Oops!</h2>
-      <p>Something went wrong. Please try again later.</p>
-      {/* Add your error handling content */}
+      <h2>Oops page</h2>
     </div>
   );
 }
